02659c5faed385535a6b6a3f61458b81b1919059
Raymond Li <hi@raymond.li>
2018-05-30 12:16:53 -0400

    Created Enum SquareTypes

---
+++ src/SquareTypes.java
@@ -0,0 +1,8 @@
+/*
+ * Authors: Raymond Li, David Tuck
+ * Date created: 2018-04-18
+ * Description: Enum types used in Minesweeper game to represent the different contents of each square
+ */
+public enum SquareTypes {
+	UNKNOWN, MISS, HIT, SUNK;
+}
