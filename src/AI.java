472268e50c0136d60ec297496227653342eb794b
Raymond Li <hi@raymond.li>
2018-05-30 12:40:36 -0400

    Clarified code

--- src/AI.java
+++ src/AI.java
@@ -9,21 +9,13 @@ public class AI {
 	 * Generates the initial population distribution density graph for the given
 	 * number of ships. Runs only at the beginning of the game.
 	 */
-<<<<<<< HEAD
-	public static void generateHeatMap() {
-=======
 	public static void generatePDDG() {
->>>>>>> 72034e7a33f77adbbf4449de8ff0bf2240c7c205
 		for (int i = 0; i < Battleship.numberOfShips; i++) {
 
 		}
 	}
-<<<<<<< HEAD
 
 	public static void generateHomeGrid() {
 
 	}
 }
-=======
-}
->>>>>>> 72034e7a33f77adbbf4449de8ff0bf2240c7c205
