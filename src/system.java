8d2112df973b91af09b919967c129c310636a44e
Raymond Li <hi@raymond.li>
2018-06-08 12:33:38 -0400

    updates

--- src/system.java
+++ src/system.java
@@ -5,36 +5,43 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.*;
+
 public class system extends JFrame {
-	MouseListener directory = new MouseListener(){
+	MouseListener directory = new MouseListener() {
 		public void mouseClicked(MouseEvent event) {
 			Object source = event.getSource();
-			if(source.equals(baseInter.gameButton)){
+			if (source.equals(baseInter.gameButton)) {
 				System.out.println(-1);
 				remove(baseInter);
 				add(gameInter);
 				repaint();
 			}
 		}
+
 		public void mouseEntered(MouseEvent e) {
 		}
+
 		public void mouseExited(MouseEvent e) {
 		}
+
 		public void mousePressed(MouseEvent e) {
 		}
+
 		public void mouseReleased(MouseEvent e) {
 		}
 	};
 	base baseInter = new base();
 	game gameInter = new game();
-	public system(){
+
+	public system() {
 		baseInter.gameButton.addMouseListener(directory);
 		add(baseInter);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
-		setSize(1300,700);
+		setSize(1300, 700);
 		setVisible(true);
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		system theGame = new system();
