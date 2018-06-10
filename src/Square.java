2f2c1658d0ad7e0c1e603607af62cd7c943819ec
Raymond Li <hi@raymond.li>
2018-06-10 16:29:50 -0400

    Removed unnecessary and unused variables, methods, housekeeping

--- src/Square.java
+++ src/Square.java
@@ -1,20 +1,15 @@
-
 /*
  * Authors: Raymond Li, David Tuck
  * Date created: 2018-05-30
  * Description: Battleship squares
  */
-import java.io.Serializable;
 
-public class Square implements Serializable {
+public class Square {
 
-	private static final long serialVersionUID = -4896377842037028342L;
-
-	public int graphArray[] = new int[Battleship.shipLengths.length];
 	public int totalSquarePD, huntPDx, huntPDy, targetPDx, targetPDy;
 	public SquareTypes status;
 	public int x, y;
-	public int shipType; // #0 for no ship, #n for n-length ship
+	public Ship shipType; // #0 for no ship, #n for n-length ship
 
 	/**
 	 * sets contains to EMPTY
@@ -23,10 +18,6 @@ public class Square implements Serializable {
 		status = SquareTypes.UNKNOWN;
 		x = startX;
 		y = startY;
-	}
-
-	public int[] returnGraphArray() {
-		return graphArray;
 	}
 
 	public void combinehuntPDXY() {
