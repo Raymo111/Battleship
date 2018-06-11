c28cf6436512be6d4aae56ce805cd37825e345b2
Raymond Li <hi@raymond.li>
2018-06-11 12:43:16 -0400

    Added break and now the cruiser is back!

--- src/Ship.java
+++ src/Ship.java
@@ -46,6 +46,7 @@ public class Ship {
 					&& !Battleship.usedShipNames.contains(Battleship.shipNames[i])) {
 				shipName = Battleship.shipNames[i];
 				Battleship.usedShipNames.add(shipName);
+				break;
 			}
 	}
 
