ef1623a75e81d62fba60ced711bdc332e6dec30b
yichunShen <yichunbshen@gmail.com>
2018-06-11 10:45:45 -0300

    Bug found
    see line 108

--- src/Battleship.java
+++ src/Battleship.java
@@ -105,6 +105,7 @@ public class Battleship implements java.io.Serializable {
 			} else {// Hit
 				for (int i = 0; i < homeShips.length; i++)
 					for (int j = 0; j < homeShips[i].location.length; j++)
+						//input: 1, You,ENTER, A1: NullPointerException
 						if (homeShips[i].location[j] == userShot) {
 							ship = homeShips[i];
 							i = shipNumber;
