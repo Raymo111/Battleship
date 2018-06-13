5c36e903d3d690fb4169fba4b389f98cab82fa71
Raymond Li <hi@raymond.li>
2018-06-13 11:00:55 -0400

    Formatted Test.java, GUI crashes with number format exception on login with username ARCHANGEL

--- src/Test.java
+++ src/Test.java
@@ -10,40 +10,41 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.*;
 import javax.swing.*;
+
 public class Test {
-	
-    public static void main(String args[]) throws Exception {
-    	JButton a = new JButton();
-    	a.addActionListener(new ActionListener(){
+
+	public static void main(String args[]) throws Exception {
+		JButton a = new JButton();
+		a.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				System.out.println("lol");
 			}
-    		
-    	});
-    	a.doClick();
-//        JFrame frame = new JFrame("Rotation Test");
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//        final BufferedImage bi = ImageIO.read(new File("gCarrier.png"));
-//        frame.add(new JPanel() {
-//
-//            @Override
-//            public Dimension getPreferredSize() {
-//                return new Dimension(bi.getWidth(), bi.getHeight());
-//            }
-//
-//            @Override
-//            protected void paintComponent(Graphics g) {
-//                super.paintComponent(g);
-//                Graphics2D g2 = (Graphics2D) g;
-//                g2.rotate(Math.PI / 2, bi.getWidth() / 2, bi.getHeight() / 2);
-//                g2.drawImage(bi, 0, 0, null);
-//            }
-//        });
-//        frame.pack();
-//        frame.setVisible(true);
-    }
+
+		});
+		a.doClick();
+		// JFrame frame = new JFrame("Rotation Test");
+		// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//
+		// final BufferedImage bi = ImageIO.read(new File("gCarrier.png"));
+		// frame.add(new JPanel() {
+		//
+		// @Override
+		// public Dimension getPreferredSize() {
+		// return new Dimension(bi.getWidth(), bi.getHeight());
+		// }
+		//
+		// @Override
+		// protected void paintComponent(Graphics g) {
+		// super.paintComponent(g);
+		// Graphics2D g2 = (Graphics2D) g;
+		// g2.rotate(Math.PI / 2, bi.getWidth() / 2, bi.getHeight() / 2);
+		// g2.drawImage(bi, 0, 0, null);
+		// }
+		// });
+		// frame.pack();
+		// frame.setVisible(true);
+	}
 }
